e0afd50e032bf34845176ac84e9b57ce31e7cdf5
Peter Mbanugo <p.mbanugo@yahoo.com>
2021-09-03 19:21:28 +0200

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,7 +1,7 @@
 const app = require("express")()
 const server = require("http").createServer(app)
 const port = process.env.PORT || "8080"
-const message = process.env.TARGET || 'Welcome to Buildpacks! This should work.'
+const message = process.env.TARGET || 'Welcome to'
 
 app.get('/', (req, res) => res.send(message))
 server.listen(port, function () {
