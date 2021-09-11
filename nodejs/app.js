a48b2f85e58666c2c62030c18ea726f6858c15e0
Peter Mbanugo <p.mbanugo@yahoo.com>
2021-09-11 17:04:51 +0200

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,7 +1,7 @@
 const app = require("express")()
 const server = require("http").createServer(app)
 const port = process.env.PORT || "8080"
-const message = "Hey! " + process.env.TARGET || 'World'
+const message = "Hey! " + process.env.TARGET || 'World.'
 
 app.get('/', (req, res) => res.send(message))
 server.listen(port, function () {
